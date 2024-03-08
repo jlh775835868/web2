e3dad4e55b9c9b7d3dc421763ac273caf88c4ba1
J <k775835868@163.com>
2024-03-08 15:02:23 +0800

    1

---
+++ 贪吃蛇/script.js
@@ -0,0 +1,18 @@
+#game-board {
+  position: relative;
+  width: 400px;
+  height: 400px;
+  border: 1px solid #000;
+  margin: 20px auto;
+}
+
+.snake, .food {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  background-color: #00ff00;
+}
+
+.food {
+  background-color: #ff0000;
+}	
